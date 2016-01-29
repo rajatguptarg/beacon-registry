d21e9c5bd25ef63123172f67004cc8fe2868ae57
Rajat Gupta <rajat.gupta712@gmail.com>
2016-01-29 11:54:59 +0530

    #303 - Rajat - Fixed logging mechanism to stream handler

--- beacons/__init__.py
+++ beacons/__init__.py
@@ -1,18 +1,10 @@
 from flask import Flask
 import logging
-from logging import FileHandler, Formatter
+from logging import Formatter
 from beacons.portal.view import portal
 
 
 app = Flask(__name__)
-
-app.config['LOG_FILE'] = 'application.log'
-file_handler = FileHandler(app.config['LOG_FILE'])
-file_handler.setLevel(logging.INFO)
-app.logger.addHandler(file_handler)
-file_handler.setFormatter(Formatter(
-    '%(asctime)s %(levelname)s: %(message)s '
-    '[in %(pathname)s:%(lineno)d]'))
 
 log_handler = logging.StreamHandler()
 log_handler.setLevel(logging.DEBUG)
@@ -28,4 +20,5 @@ log_handler.setFormatter(Formatter(
     '\n-------------------------------------\n'))
 
 app.logger.addHandler(log_handler)
+
 app.register_blueprint(portal)
