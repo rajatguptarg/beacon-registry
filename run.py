8f09cf822b43f0dae8280281b1d2486f1dbae5ab
Rajat Gupta <rajat.gupta712@gmail.com>
2016-04-19 10:32:51 +0530

    #667 - Rajat - Checking for env variable also

--- run.py
+++ run.py
@@ -10,7 +10,8 @@ from beacons import app
 
 if __name__ == '__main__':
     argument, _ = map(str, sys.argv[1].split('='))
-    if argument == 'config_directory':
+    env, _ = map(str, sys.argv[2].split('='))
+    if argument == 'config_directory' and env == 'env':
         app.secret_key = str(uuid.uuid4())
         app.run(debug=True, port=9020, host='0.0.0.0')
     else:
