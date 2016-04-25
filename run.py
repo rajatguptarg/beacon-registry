45940ca8f7727ec20b79ec1a133417edf6c378e4
Rajat Gupta <rajat.gupta712@gmail.com>
2016-04-25 22:04:45 +0530

    #667 - Rajat - Running on port 443

--- run.py
+++ run.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     env, _ = map(str, sys.argv[2].split('='))
     if argument == 'config_directory' and env == 'env':
         app.secret_key = str(uuid.uuid4())
-        app.run(debug=True, port=9020, host='0.0.0.0')
+        app.run(debug=True, port=443, host='0.0.0.0')
     else:
         raise ValueError
